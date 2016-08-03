eef6773f936aa16d02069f836f26aee286bc6548
nielsenpuma <nielsenpumajihuallanca@MacBook-Pro-de-Nielsen.local>
2016-08-02 23:44:56 -0500

    Creando una ClasePrueba para Singleton

---
+++ PatronesDisenho/src/pkg_Singleton/PruebasSingleton.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg_Singleton;
+
+/**
+ *
+ * @author nielsenpumajihuallanca
+ */
+public class PruebasSingleton {
+    public static void main(String[] args) {
+        Singleton miSingleton = Singleton.obtenerSingleton();
+        Singleton miSingleton2 = Singleton.obtenerSingleton();
+        Singleton miSingleton3 = Singleton.obtenerSingleton();
+        Singleton miSingleton4 = Singleton.obtenerSingleton();
+        Singleton miSingleton5 = Singleton.obtenerSingleton();
+        Singleton miSingleton6 = Singleton.obtenerSingleton();
+        Singleton miSingleton7 = Singleton.obtenerSingleton();
+        Singleton miSingleton8 = Singleton.obtenerSingleton();
+        Singleton miSingleton9 = Singleton.obtenerSingleton();
+        Singleton miSingleton10 = Singleton.obtenerSingleton();
+        System.out.println("Fin de la creación");
+    }
+    
+}
