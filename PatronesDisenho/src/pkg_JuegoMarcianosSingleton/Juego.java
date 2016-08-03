d380d293df2c08dc84d1feffb94a898d6ba8d46d
nielsenpuma <nielsenpumajihuallanca@MacBook-Pro-de-Nielsen.local>
2016-08-03 00:34:32 -0500

    Clase Juego
    Para ejecutar el juego de los marcianos y ver el funcionamiento de
    Singleton Marcianos

---
+++ PatronesDisenho/src/pkg_JuegoMarcianosSingleton/Juego.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg_JuegoMarcianosSingleton;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nielsenpumajihuallanca
+ */
+public class Juego {
+    
+    
+    public static void main(String[] args) {
+        Marcianos marcianos;
+        Computadora computadora;
+        Jugador jugador;
+        Scanner sc;
+        
+        marcianos = Marcianos.getMarcianos();
+        computadora = new Computadora();
+        jugador = new Jugador();
+        sc = new Scanner(System.in);
+        
+        int disparos;
+        
+        do {
+            System.out.println("Disparos que va a realizar: ");
+            disparos = sc.nextInt();
+            for(int i = 0; i<disparos; i++){
+                jugador.destruirMarciano();
+            }
+            computadora.crearMarcianos();
+        } while (disparos != 0);
+    }
+}
