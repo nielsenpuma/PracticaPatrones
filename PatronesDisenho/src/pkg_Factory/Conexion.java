aef98f7ccc55f3f6e62a4c3d347567cb39623361
nielsenpuma <nielsenpumajihuallanca@MacBook-Pro-de-Nielsen.local>
2016-08-06 17:55:47 -0500

    Clase abstracta conexión
    Clase abstracta de conexión el cual tendrá que ser implementada en otro
    según el tipo de datos.

---
+++ PatronesDisenho/src/pkg_Factory/Conexion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg_Factory;
+
+/**
+ *
+ * @author nielsenpumajihuallanca
+ */
+public abstract class Conexion {
+    //Constructor vacío
+    public Conexion(){
+        
+    }
+    
+    //Método descripción
+    public String descripcion(){
+        return "Conexión Genérica";
+    }
+}
