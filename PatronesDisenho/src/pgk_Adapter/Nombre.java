01fd5a62428c393bec6e6dad339bd03f40e99e65
nielsenpuma <nielsenpumajihuallanca@MBP-de-Nielsen.home>
2016-08-13 18:37:00 -0500

    Clase Nombre
    Clase que supuestamente desconocida el cual solo conocemos su método
    get y set de NombreCompuesto()

---
+++ PatronesDisenho/src/pgk_Adapter/Nombre.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pgk_Adapter;
+
+/**
+ *
+ * @author nielsenpumajihuallanca
+ */
+public class Nombre {
+    private String NombreCompuesto;
+
+    public Nombre() {
+    }
+
+    public String getNombreCompuesto() {
+        return NombreCompuesto;
+    }
+
+    public void setNombreCompuesto(String NombreCompuesto) {
+        this.NombreCompuesto = NombreCompuesto;
+    }
+    
+    
+}
