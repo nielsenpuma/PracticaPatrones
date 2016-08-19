44a93e964766b61b3952d1926e104ba7fd55a2d2
nielsenpuma <nielsenpumajihuallanca@MacBook-Pro-de-Nielsen.local>
2016-08-19 17:10:47 -0500

    Interface I_ImpLista
    Implementación base para las listas, ene l cual en las otras se
    sobrescribirá sus métodos.

---
+++ PatronesDisenho/src/pgk_Bridge/I_ImplLista.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pgk_Bridge;
+
+/**
+ *
+ * @author nielsenpumajihuallanca
+ */
+public interface I_ImplLista {
+    public void addItem(String item);
+    public void remItem(String item);
+    public int getCantidadDeItems();
+    public String getitem(int index);
+    public boolean soportaRepetidos();
+}
