b5371b9d2be232bff894c95ccc9ed160f0f69579
nielsenpuma <nielsenpumajihuallanca@MacBook-Pro-de-Nielsen.local>
2016-08-22 15:09:41 -0500

    Clase abstracta DecoradorHamburguesa
    Clase el cual de aquí todas las hijas tendrán que sobrescribir el
    método.

---
+++ PatronesDisenho/src/pgk_Decorator/DecoradorHamburguesa.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pgk_Decorator;
+
+/**
+ *
+ * @author nielsenpumajihuallanca
+ */
+public abstract class DecoradorHamburguesa extends Hamburguesa{
+     public abstract String getDescripcion();
+}
