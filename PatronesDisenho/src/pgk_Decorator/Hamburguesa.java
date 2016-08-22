59dffc0f57635d1ceeafe4067c0a35161e78cdb8
nielsenpuma <nielsenpumajihuallanca@MacBook-Pro-de-Nielsen.local>
2016-08-22 15:05:40 -0500

    Clase Hamburguesa
    La clase base para el patron Decorador.

---
+++ PatronesDisenho/src/pgk_Decorator/Hamburguesa.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pgk_Decorator;
+
+/**
+ *
+ * @author nielsenpumajihuallanca
+ */
+public class Hamburguesa {
+    public String getDescripcion(){
+        return "Carne + Pan";
+    }
+}
