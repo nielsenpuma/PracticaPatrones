fdacf30628780e2470ba548bb320f5eb57ab1c7f
nielsenpuma <nielsenpumajihuallanca@MacBook-Pro-de-Nielsen.local>
2016-08-22 18:46:13 -0500

    Interface Observador
    Donde se sobrescribirán todos los demás observadores.

---
+++ PatronesDisenho/src/pgk_Observer/Observador.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pgk_Observer;
+
+/**
+ *
+ * @author nielsenpumajihuallanca
+ */
+public interface Observador {
+    public void actualizar(String accion, String lugar);
+}
