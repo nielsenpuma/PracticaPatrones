7f4f2940784ef323a0ab2c6c72b4de8f9c324383
nielsenpuma <nielsenpumajihuallanca@MacBook-Pro-de-Nielsen.local>
2016-08-22 22:26:28 -0500

    Clase Algoritmo
    Donde se implementarán todos los algoritmos necesarios para mover el
    avión.

---
+++ PatronesDisenho/src/pgk_Strategy/Algoritmo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pgk_Strategy;
+
+/**
+ *
+ * @author nielsenpumajihuallanca
+ */
+public interface Algoritmo {
+    public void moverse(); 
+}
