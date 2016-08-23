b9a2c76c5fbd6116788e82988903744848f7f3e6
nielsenpuma <nielsenpumajihuallanca@MacBook-Pro-de-Nielsen.local>
2016-08-22 21:12:40 -0500

    Interface IntercaceAyuda
    Donde se implementará todas las clases hijas.

---
+++ PatronesDisenho/src/pgk_ChainOfResponsability/InterfaceAyuda.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pgk_ChainOfResponsability;
+
+/**
+ *
+ * @author nielsenpumajihuallanca
+ */
+public interface InterfaceAyuda {
+    public void getAyuda(int tipoAyuda);
+}
